16845d440d7c19778e2f2f5fce5f3b7406db9e75
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-09-23 21:54:28 +0100

    adds zsh and more packages

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
+# !/bin/bash
+#
 # Author:      JoaoAJMatos (https://github.com/JoaoAJMatos)
 # Date:        09/23/2023
+# 
 # Description: This shell script sets up a new Ubuntu system for my development workflow,
 #              installing everything I need to start working on my projects.
 
@@ -9,7 +12,29 @@ cd ~ && sudo apt-get update
 sudo apt-get install curl -y        # Curl
 sudo apt-get install neofetch -y    # Neofetch bc yeah...
 sudo apt-get install xclip -y       # Tool to handle clipboard through the terminal
-sudo apt-get install python3-pip -y # Pip to download Getgist
+sudo apt-get install htop -y        # Htop to monitor system resources
+sudo apt-get install tree -y        # Tree to display directory structure
+sudo apt-get install wget -y        # Wget to download files from the web
+sudo apt-get install net-tools -y   # Net-tools to use ifconfig
+sudo apt-get install cmake -y
+sudo apt-get install build-essential -y
+sudo apt-get install python3-dev -y
+sudo apt-get install python3-setuptools -y
+sudo apt-get install python3-wheel -y
+sudo apt-get install python3-venv -y
+sudo apt-get install python3-pip -y
+
+# Install latest version of neovim
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt-get update && sudo apt-get install neovim -y
+
+# Install latest version of tmux
+sudo add-apt-repository ppa:pi-rho/dev -y
+sudo apt-get update && sudo apt-get install tmux -y
+
+# Install latest version of NodeJS
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
 
 # Install latest Git
 sudo add-apt-repository ppa:git-core/ppa -y
@@ -41,8 +66,11 @@ ssh-keygen -t rsa -b 4096 -C $git_config_email
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 
-# Install ZSH
+# Setup ZSH and Oh My ZSH
 sudo apt-get install zsh -y
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+getmy .zshrc
 
 # Install Discord
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
